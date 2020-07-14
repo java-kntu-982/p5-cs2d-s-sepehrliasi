6645c9434f494671658799b6b304a692741c19ff
Sepehr Liasi <sepehrghaseminejad@yahoo.com>
2020-07-15 02:03:37 +0430

    Socket testing

--- src/main/java/ir/ac/kntu/cs2d/client/ClientMain.java
+++ src/main/java/ir/ac/kntu/cs2d/client/ClientMain.java
@@ -1,11 +1,42 @@
 package ir.ac.kntu.cs2d.client;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ClientMain {
-    public static void main(String[] args) {
-        // TODO
+    public static void main(String[] args) throws Exception{
+        Socket socket = new Socket("localhost",9999);
+
+        //send
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+        printWriter.println("hello");
+        printWriter.flush();
+
+        //get
+        InputStreamReader in = new InputStreamReader(socket.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+        String str = bf.readLine();
+        System.out.println("server: " + str);
+
+        Scanner input = new Scanner(System.in);
+        String client = input.nextLine();
+        String compare = "";
+
+        while(client.compareTo(compare) != 0){
+            printWriter = new PrintWriter(socket.getOutputStream());
+            printWriter.println(client);
+            printWriter.flush();
+            in = new InputStreamReader(socket.getInputStream());
+            bf = new BufferedReader(in);
+            str = bf.readLine();
+            System.out.println("server: " + str);
+            client = input.nextLine();
+        }
     }
 
     private ClientMain() {
-
     }
 }
--- src/main/java/ir/ac/kntu/cs2d/server/ServerMain.java
+++ src/main/java/ir/ac/kntu/cs2d/server/ServerMain.java
@@ -1,8 +1,47 @@
 package ir.ac.kntu.cs2d.server;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ServerMain {
-    public static void main(String[] args) {
-        // TODO
+    public static void main(String[] args) throws Exception{
+        System.out.println("server started");
+        ServerSocket serverSocket = new ServerSocket(9999);
+        System.out.println("waiting for client ...");
+        Socket socket = serverSocket.accept();
+        System.out.println("client connected");
+
+        //get
+        InputStreamReader in = new InputStreamReader(socket.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+        String str = bf.readLine();
+
+        //send
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+        printWriter.println("hello");
+        printWriter.flush();
+
+        String server;
+        Scanner input = new Scanner(System.in);
+        String compare = "";
+
+        int t = 0;
+        while (t == 0) {
+            if(str.compareTo(compare) != 0) {
+                compare = str;
+                System.out.println("client: " + str);
+                in = new InputStreamReader(socket.getInputStream());
+                bf = new BufferedReader(in);
+                str = bf.readLine();
+                server = input.nextLine();
+                printWriter.println(server);
+                printWriter.flush();
+            }
+        }
     }
 
     private ServerMain() {
